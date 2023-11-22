7242d1bbe9f745f583fa6814dc2d147dce58c277
Patrick <116662812+pwheelerRio@users.noreply.github.com>
2023-11-22 16:04:43 -0700

    Initial Commit

---
+++ JazzPHX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JazzPHX
+//
+//  Created by Patrick Wheeler on 11/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JazzPHX/JazzPHXApp.swift
@@ -0,0 +1,17 @@
+//
+//  JazzPHXApp.swift
+//  JazzPHX
+//
+//  Created by Patrick Wheeler on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct JazzPHXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
