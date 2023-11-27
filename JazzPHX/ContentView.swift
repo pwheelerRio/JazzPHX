5fa7454b13e6920a2d918e7722b5ca30ba2b297c
Patrick <116662812+pwheelerRio@users.noreply.github.com>
2023-11-26 18:42:59 -0700

    Added Tabbed Navigation

--- JazzPHX/ContentView.swift
+++ JazzPHX/ContentView.swift
@@ -8,18 +8,52 @@
 import SwiftUI
 import AVKit
 
-var myVariable: Int = 10
+//var myVariable: Int = 10
 var streamPlayer: AVPlayer?
-let audioURL = URL(string: "https://kjzz.streamguys1.com/kjzzhd2_mp3_128")!
+let jazzStreamURL = URL(string: "https://kjzz.streamguys1.com/kjzzhd2_mp3_128")!
 
 struct ContentView: View {
     
     @State private var isPlaying = false
-    
+    @State private var iconAnimate = false
     
     var body: some View {
-//        var streamPlayer: AVPlayer?
-        
+        TabView {
+            LiveRadioTab()
+                .tabItem {
+                    Image(systemName: "music.note.list")
+//                        .symbolEffect(.bounce, value: iconAnimate  )
+//                        .onTapGesture {
+//                            iconAnimate.toggle()
+//                        }
+                        // this does not appear to do anything.
+                    // TODO - how do I animate these?
+                    Text("Jazz Radio")
+                }
+               
+            
+            JazzEventsTab()
+                .tabItem {
+                    Image(systemName: "music.mic")
+                    Text("Live Music Scene")
+                }
+            
+            SupportTab ()
+                .tabItem {
+                    Image(systemName: "radio")
+                    Text("Support KJZZ")
+                }
+        }
+    }
+}
+
+struct LiveRadioTab: View {
+    
+    // TODO - music plays when navigating to other tabs, but stops when user returns to Live Radio tab.
+    
+    @State private var isPlaying = false
+    
+    var body: some View {
         VStack {
             Text("Jazz PHX")
                 .font(.title)
@@ -33,66 +67,64 @@ struct ContentView: View {
             }) {
                 Image(systemName: isPlaying ? "pause.circle.fill" : "play.circle.fill")
                     .font(.system(size: 100))
-                    .foregroundColor(isPlaying ? .red : .green)
+                    .foregroundColor(isPlaying ? .red : .blue)
+                // TODO - should be able to animate the play button too
             }
             .padding()
         }
         .onAppear {
-            //            self.setupAudioPlayer()
-            //            self.audioPlayer = try! AVAudioPlayer
-            
-//            setupStreamPlayer()
-            
-            myVariable = 12
-            print(myVariable)
-            
             setupStreamPlayer()
-            print(myVariable)
         }
         .onChange(of: isPlaying) {
-            //            setupAudioPlayer()
             if isPlaying {
-                print("Play")
+//                print("Play")
                 streamPlayer?.play()
             } else {
-                print("Stop")
+//                print("Stop")
                 streamPlayer?.pause()
             }
         }
     }
     
     func setupStreamPlayer() {
-        myVariable = 15
         do {
             try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
-            streamPlayer = AVPlayer(url: audioURL)
-    } catch { }
-        
+            streamPlayer = AVPlayer(url: jazzStreamURL)
+        } catch { 
+            print("Error setting up audio session")
+        }
     }
-    //    private func setupAudioPlayer() {
-    //        let playerItem = AVPlayerItem(url: URL(string: "https://kjzz.streamguys1.com/kjzzhd2_mp3_128")!)
-    //        print(audioURL)
-    //        let player = AVPlayer(playerItem: playerItem)
-    //
-    //        if isPlaying {
-    //            print("Play")
-    //            player.play()
-    //        } else {
-    //            print("Stop")
-    //            player.pause()
-    //        }
-    //    }
-    //
+}
+
+struct JazzEventsTab: View {
     
+    var body: some View {
+        VStack {
+            Text("Tab would display information about local live music scene/events")
+                .font(.title)
+                .padding()
+        }
+    }
+}
+
+struct SupportTab: View {
+    
+    var body: some View {
+        VStack {
+            Text("Tab would give information about station and supporting KJZZ")
+                .font(.title)
+                .padding()
+        }
+    }
 }
 
 // part of tutorial
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
-
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
+//
 // part of template
 #Preview {
     ContentView()
