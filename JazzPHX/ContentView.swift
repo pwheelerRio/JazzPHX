f9d87e3e7f372a4c79b4d153c0cb81c369f23c1f
Patrick <116662812+pwheelerRio@users.noreply.github.com>
2023-11-26 17:19:31 -0700

    Initial push. Playback is working.

--- JazzPHX/ContentView.swift
+++ JazzPHX/ContentView.swift
@@ -6,19 +6,94 @@
 //
 
 import SwiftUI
+import AVKit
+
+var myVariable: Int = 10
+var streamPlayer: AVPlayer?
+let audioURL = URL(string: "https://kjzz.streamguys1.com/kjzzhd2_mp3_128")!
 
 struct ContentView: View {
+    
+    @State private var isPlaying = false
+    
+    
     var body: some View {
+//        var streamPlayer: AVPlayer?
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Jazz PHX")
+                .font(.title)
+                .padding()
+            
+            
+            Button(action: {
+                self.isPlaying.toggle()
+                
+                print(isPlaying)
+            }) {
+                Image(systemName: isPlaying ? "pause.circle.fill" : "play.circle.fill")
+                    .font(.system(size: 100))
+                    .foregroundColor(isPlaying ? .red : .green)
+            }
+            .padding()
         }
-        .padding()
+        .onAppear {
+            //            self.setupAudioPlayer()
+            //            self.audioPlayer = try! AVAudioPlayer
+            
+//            setupStreamPlayer()
+            
+            myVariable = 12
+            print(myVariable)
+            
+            setupStreamPlayer()
+            print(myVariable)
+        }
+        .onChange(of: isPlaying) {
+            //            setupAudioPlayer()
+            if isPlaying {
+                print("Play")
+                streamPlayer?.play()
+            } else {
+                print("Stop")
+                streamPlayer?.pause()
+            }
+        }
+    }
+    
+    func setupStreamPlayer() {
+        myVariable = 15
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+            streamPlayer = AVPlayer(url: audioURL)
+    } catch { }
+        
+    }
+    //    private func setupAudioPlayer() {
+    //        let playerItem = AVPlayerItem(url: URL(string: "https://kjzz.streamguys1.com/kjzzhd2_mp3_128")!)
+    //        print(audioURL)
+    //        let player = AVPlayer(playerItem: playerItem)
+    //
+    //        if isPlaying {
+    //            print("Play")
+    //            player.play()
+    //        } else {
+    //            print("Stop")
+    //            player.pause()
+    //        }
+    //    }
+    //
+    
+}
+
+// part of tutorial
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
 
+// part of template
 #Preview {
     ContentView()
 }
